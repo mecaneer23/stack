d897731537264f8bad436b908ec55f083ff1f9c1
mecaneer23 <mecaneer23@gmail.com>
2023-10-30 13:31:48 -0500

    begin to adhere to best practices

--- stack.py
+++ stack.py
@@ -1,37 +1,44 @@
 #!/usr/bin/env python3
+#pylint: disable=missing-module-docstring
+#pylint: disable=missing-function-docstring, no-member, invalid-name
 
 import curses
+from typing import Any
 
 
-def main(stdscr):
+def init(stdscr: Any) -> None:
     curses.curs_set(0)
     curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_GREEN, -1)
     curses.init_pair(2, curses.COLOR_RED, -1)
+    stdscr.nodelay(True)
+    stdscr.timeout(100)
+
+
+def main(stdscr: Any) -> None:
+    init(stdscr)
     colors = {
         "horizontal": 1,
         "vertical": 2,
     }
-    stdscr.nodelay(True)
-    stdscr.timeout(100)
     vertical_char = "*"
     horizontal_char = "#"
     score = 0
-    screen_size = min(stdscr.getmaxyx()) - 1
-    tower_y, tower_x = screen_size * 2 // 3, screen_size * 4 // 3
+    SCREEN_SIZE = min(stdscr.getmaxyx()) - 1
+    tower_y, tower_x = SCREEN_SIZE * 2 // 3, SCREEN_SIZE * 4 // 3
     last_tower_y, last_tower_x = tower_y, tower_x
     game_win = curses.newwin(
-        screen_size,
-        int(screen_size * 2),
-        stdscr.getmaxyx()[0] // 2 - screen_size // 2,
-        stdscr.getmaxyx()[1] // 2 - screen_size,
+        SCREEN_SIZE,
+        int(SCREEN_SIZE * 2),
+        stdscr.getmaxyx()[0] // 2 - SCREEN_SIZE // 2,
+        stdscr.getmaxyx()[1] // 2 - SCREEN_SIZE,
     )
     game_win.box()
     # while True:
     for i in range(tower_y):
         game_win.addstr(
-            (screen_size - tower_y) // 2 + i,
-            screen_size - tower_x // 2,
+            (SCREEN_SIZE - tower_y) // 2 + i,
+            SCREEN_SIZE - tower_x // 2,
             horizontal_char * tower_x,
             curses.color_pair(colors["horizontal"])
         )
